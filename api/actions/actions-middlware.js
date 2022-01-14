9a1b905c4a1ebb13ba07b8dc052416e7f5dd1124
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-14 15:42:35 -0600

    Completed POST task for new actions

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,5 +1,6 @@
 // add middleware here related to actions
-const Action = require('./actions-model');
+const Action = require('./actions-model.js');
+const Project = require('../projects/projects-model.js');
 
 async function checkActionId(req, res, next) {
     try {
@@ -9,11 +10,33 @@ async function checkActionId(req, res, next) {
             req.actions = possibleAction;
             next();
         } else {
-            next({ status: 404, message: `No action found with ID: ${id}` })
+            next({ status: 404, message: `No action found with ID: ${id}` });
         }
     } catch (err) {
         next(err);
     }
 }
 
-module.exports = { checkActionId }
+async function checkAction(req, res, next) {
+    try {
+        const { project_id, description, notes } = req.body;
+        if (!description || !notes || !project_id) {
+            res.status(400).json({ message: 'Please enter a project_id, description, and notes to submit a new action' });
+        } else {
+            const possibleProject = await Project.get(project_id);
+            if (!possibleProject) {
+                next({ status: 404, message: `No project found with ID: ${project_id}` });
+            } else {
+                req.project = possibleProject;
+                next();
+            }
+        }
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = { 
+    checkActionId,
+    checkAction
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,11 +2,11 @@
 const express = require('express');
 const router = express.Router();
 
-const Actions = require('./actions-model.js');
-const { checkActionId } = require('./actions-middlware.js');
+const Action = require('./actions-model.js');
+const { checkActionId, checkAction } = require('./actions-middlware.js');
 
 router.get('/', (req, res, next) => {
-    Actions.get()
+    Action.get()
         .then(actions => {
             res.status(200).json(actions);
         })
@@ -14,9 +14,17 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:id', checkActionId, (req, res, next) => {
-    Actions.get(req.params.id)
-        .then(actions => {
-            res.status(200).json(actions);
+    Action.get(req.params.id)
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(next);
+});
+
+router.post('/', checkAction, (req, res, next) => {
+    Action.insert(req.body)
+        .then(action => {
+            res.status(201).json(action);
         })
         .catch(next);
 });
