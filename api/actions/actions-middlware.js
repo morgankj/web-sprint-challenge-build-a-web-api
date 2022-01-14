b945f2894f9f4b015e567533fb2adcd3625f0e89
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-14 14:52:37 -0600

    Completed GET task for actions by ID

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,19 @@
-// add middlewares here related to actions
+// add middleware here related to actions
+const Action = require('./actions-model');
+
+async function checkActionId(req, res, next) {
+    try {
+        const id = req.params.id;
+        const possibleAction = await Action.get(id);
+        if (possibleAction) {
+            req.actions = possibleAction;
+            next();
+        } else {
+            next({ status: 404, message: `No action found with ID: ${id}` })
+        }
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = { checkActionId }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,9 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 const Actions = require('./actions-model.js');
+const { checkActionId } = require('./actions-middlware.js');
 
 router.get('/', (req, res, next) => {
     Actions.get()
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(next);
+});
+
+router.get('/:id', checkActionId, (req, res, next) => {
+    Actions.get(req.params.id)
         .then(actions => {
             res.status(200).json(actions);
         })
